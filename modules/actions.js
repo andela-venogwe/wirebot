cd70c021d67e6e71b9d7e061704b8729427359e3
Ian King'ori <ian.kingori@andela.com>
2017-11-21 12:28:55 +0300

    #151797578 log incident
    * (feat #151797578) log incidents via slack
    - add date
    - add category
    - add description
    - add location
    - tag witnesses
    
    * (chore) code cleanup

---
+++ modules/actions.js
@@ -0,0 +1,76 @@
+const tempIncidents = {};
+
+const start = (payload, respond) => {
+  console.log(payload);
+  const selected = payload.actions[0].value;
+  const userId =  payload.user.id;
+  if(selected === 'no') {
+    return respond({'text': 'Ok, let me know if you change your mind :smiley:'});
+  } else if(tempIncidents[userId]) {
+    return respond({'text': 'we already started your case'});
+  } else {
+    tempIncidents[userId] = {
+      step: 0
+    };
+    return respond({'text': "When did the incident occur? (dd-mm-yy)"});
+  }
+  
+}
+
+const saveDate = (event) => {
+  const userId = event.user;
+  const message = event.text;
+  tempIncidents[userId].step += 1;
+  //TODO: add message validation
+  tempIncidents[userId].date = message;
+
+}
+
+const saveLocation = (event) => {
+  const userId = event.user;
+  const message = event.text;
+  tempIncidents[userId].step += 1;
+  //TODO:location validation
+  tempIncidents[userId].location = message;
+
+}
+
+const saveCategory = (payload, respond) => {
+  const userId = payload.user.id;
+  tempIncidents[userId].category = payload.actions[0].value;
+  tempIncidents[userId].step += 1;
+  return respond({'text': 'Okay, What happened?'})
+}
+
+const saveDescription = (event) => {
+  const userId = event.user;
+  const message = event.text;
+  tempIncidents[userId].description = message;
+  tempIncidents[userId].step += 1;
+}
+
+const saveIncident = (payload, respond) => {
+  //TODO: hit api
+  const userId = payload.user.id;
+  delete tempIncidents[userId];
+  return respond({text: 'Incident number: WX5667F'});
+}
+
+const saveWitnesses = (event) => {
+  const userId = event.user;
+  const message = event.text;
+  tempIncidents[userId].witnesses = message;
+  tempIncidents[userId].step += 1;
+  console.log(tempIncidents);
+}
+
+module.exports = {
+  start,
+  tempIncidents,
+  saveDate,
+  saveLocation,
+  saveCategory,
+  saveDescription,
+  saveIncident,
+  saveWitnesses
+}
