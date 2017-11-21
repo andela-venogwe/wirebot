9d15080349834cf62062eb34a2348f1d4aa546ec
Ian King'ori <ian.kingori@andela.com>
2017-11-21 12:28:55 +0300

    #151797578 log incident
    * (feat #151797578) log incidents via slack
    - add date
    - add category
    - add description
    - add location
    - tag witnesses
    
    * (chore) code cleanup

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const bodyParser = require('body-parser');
 const slackInteractiveMessages = require('@slack/interactive-messages');
 const { createSlackEventAdapter } = require('@slack/events-api');
 const SlackClient = require('@slack/client').WebClient;
-const initiationMessage = require('./modules/messages').initiate; 
+
+const  { initiationMessage, categoryMessage, witnessesMessage, getConfirmationMessage } = require('./modules/messages'); 
+const actions = require('./modules/actions');
 
 const dotenv = require('dotenv');
 
@@ -16,21 +18,121 @@ const slackMessages = slackInteractiveMessages
   .createMessageAdapter(process.env.SLACK_VERIFICATION_TOKEN);
 const port = process.env.PORT || 3000;
 
+const confirmIncident = (user, channel) => {
+  const data = actions.tempIncidents[user];
+  sc.chat.postMessage(
+    channel,
+    '',
+    getConfirmationMessage(data),
+  (err, res) => {
+    // console.log(res);
+  });
+}
+
 slackEvents.on('message', (event) => {
   // do not respond to edited messages or messages from me
   if (event.subtype === 'bot_message' || event.subtype === 'message_changed') {
     return;
   };
-  console.log(event);
-  sc.chat.postMessage(
-    event.channel,
-    '',
-    initiationMessage,
-  (err, res) => {
-    console.log(res);
-  });
+
+  const userId = event.user;
+  if (!actions.tempIncidents[userId]) {
+
+    sc.chat.postMessage(
+      event.channel,
+      '',
+      initiationMessage,
+    (err, res) => {
+      // console.log(res);
+    });
+
+  } else {
+
+    const currentStep = actions.tempIncidents[userId].step;
+    switch(currentStep) {
+      case 0:
+        actions.saveDate(event);
+        sc.chat.postMessage(
+          event.channel,
+          'Where did this happen? (place, city, country)',
+        (err, res) => {
+          // console.log(res);
+        });
+        break;
+      case 1:
+        actions.saveLocation(event);
+        sc.chat.postMessage(
+          event.channel,
+          '',
+          categoryMessage,
+        (err, res) => {
+          // console.log(res);
+        });
+        break;
+      case 2:
+        console.log('Logic flaw??');
+        break;
+      case 3:
+        actions.saveDescription(event);
+        sc.chat.postMessage(
+          event.channel,
+          '',
+          witnessesMessage,
+        (err, res) => {
+          // console.log(res);
+        });
+        break;
+      case 4:
+        actions.saveWitnesses(event);
+        confirmIncident(event.user, event.channel);
+        break;
+      case 5:
+       
+        break;
+
+    }
+  }
+
 });
 
+slackMessages.action('report', (payload, respond)=> {
+  actions.start(payload, respond);
+  return {
+    text: 'Just a sec'
+  };
+});
+
+slackMessages.action('category', (payload, respond)=> {
+  actions.saveCategory(payload, respond);
+  return {
+    text: 'Just a sec'
+  };
+});
+
+slackMessages.action('confirm', (payload, respond)=> {
+  actions.saveIncident(payload, respond);
+  return {
+    text: 'Your incident has been logged'
+  };
+});
+
+slackMessages.action('witnesses', (payload, respond)=> {
+  const selected = payload.actions[0].value;
+  if (selected === 'no') {
+    // show confirmation
+    const event = {
+      user: payload.user.id,
+      text: ''
+    }
+    actions.saveWitnesses(event);
+    confirmIncident(payload.user.id, payload.channel.id);
+  } else {
+    respond({'text': 'Who are your witnesses? (@witness1, @witness2)'});
+  }
+  return {
+    text: 'Just a sec'
+  };
+});
 
 // Start a basic HTTP server
 const app = express();
--- modules/messages.js
+++ modules/messages.js
@@ -3,13 +3,133 @@ const initiate = {
     {
       color: '#5A352D',
       title: 'Do you want to report an incident?',
-      callback_id: 'log',
+      callback_id: 'report',
       actions: [
         {
           name: 'yes',
           text: 'Yes',
           type: 'button',
-          value: 'report',
+          value: 'yes',
+        },
+        {
+          name: 'no',
+          text: 'No',
+          type: 'button',
+          value: 'no',
+        },
+      ],
+    },
+  ],
+};
+
+const categoryMessage = {
+  attachments: [
+    {
+      "color": "#5A352D",
+      "title": "What category does the incident belong to?",
+      "callback_id": "category",
+      "actions" : [
+        {
+          "name": "red",
+          "text": "Red",
+          "type": "button",
+          "value": "red",
+          "style": "danger"
+        },
+        {
+          "name": "yellow",
+          "text": "Yellow",
+          "type": "button",
+          "value": "yellow"
+        },
+        {
+          "name": "green",
+          "text": "Green",
+          "type": "button",
+          "value": "green"
+        },
+        {
+          "name": "other",
+          "text": "I don't know",
+          "type": "button",
+          "value": "other"
+        }
+      ]
+    }
+  ]
+};
+
+const getDateMessage = {
+  text: "When did the incident occur? (dd-mm-yy)"
+};
+
+const getConfirmationMessage = (data) =>
+{
+  return {
+   "attachments": [
+     {
+       "color": "#36a64f",
+       "pretext": "Please confirm the incident",
+       "callback_id": "confirm",
+       "fields": [
+           {
+               "title": "Category",
+               "value": data.category,
+               "short": false
+           },
+           {
+               "title": "Date",
+               "value": data.date,
+               "short": false
+           },
+           {
+               "title": "Location",
+               "value": data.location,
+               "short": false
+           },
+           {
+               "title": "Description",
+               "value": data.description,
+               "short": false
+           },
+           {
+               "title": "Witnesses",
+               "value": data.witnesses,
+               "short": false
+           }
+         ],
+       "actions" : [
+         {
+           "name": "confirm",
+           "text": "Confirm",
+           "type": "button",
+           "value": "confirm",
+           "style": "ok"
+         },
+         {
+           "name": "cancel",
+           "text": "Cancel",
+           "type": "button",
+           "value": "cancel"
+         }
+       ]
+     }
+   ]
+ };
+}
+
+const witnessesMessage = {
+  attachments: [
+    {
+      color: '#5A352D',
+      title: 'Do you have any witnesses?',
+      callback_id: 'witnesses',
+      actions: [
+        {
+          name: 'yes',
+          text: 'Yes',
+          type: 'button',
+          value: 'yes',
         },
         {
           name: 'no',
@@ -24,6 +144,10 @@ const initiate = {
 
 
 module.exports = {
-  initiate
+  initiationMessage: initiate,
+  getDateMessage,
+  getConfirmationMessage,
+  categoryMessage,
+  witnessesMessage
 };
 
