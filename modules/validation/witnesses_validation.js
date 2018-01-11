44f7524584b30d6ff1ba069821398fd6f00b03e1
andela-bm <bmuthoga@users.noreply.github.com>
2018-01-11 11:57:54 +0300

    feat(witnesses-validation): validate witnesses entered by a user when reporting an incident (#9)
    - add 'modules/validation/witnesses_validation/isWitnessValid()' to validate if witness entered is in valid format and exists
    - implement above in 'index.js'
    
    [Delivers #152106921]

---
+++ modules/validation/witnesses_validation.js
@@ -0,0 +1,21 @@
+const isWitnessValid = entered_witnesses => {
+  entered_witnesses_array = entered_witnesses.split(",");
+
+  regex_test_result_array = [];
+
+  entered_witnesses_array.forEach(entered_witness => {
+    entered_witness = entered_witness.trim();
+
+    regex_test_result_array.push(/^<@[A-Z0-9]*>$/.test(entered_witness));
+  });
+
+  if (regex_test_result_array.includes(false) === true) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+module.exports = {
+  isWitnessValid
+};
