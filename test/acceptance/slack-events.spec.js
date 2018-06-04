ad6c5adff62ca46ae0d0927502044876e7deeb58
iykyvic <victor.enogwe@gmail.com>
2018-06-04 18:13:01 +0100

    chore(tests): add dummy slack events test

---
+++ test/acceptance/slack-events.spec.js
@@ -0,0 +1,15 @@
+import {
+  describe,
+  it,
+  should
+} from '../helpers';
+
+describe('Slack Events:', () => {
+  describe('Message', () => {
+    describe('Bot Message And Message Changed: ', () => {
+      it('should do something', () => {
+        should.exist(2);
+      });
+    });
+  });
+});
